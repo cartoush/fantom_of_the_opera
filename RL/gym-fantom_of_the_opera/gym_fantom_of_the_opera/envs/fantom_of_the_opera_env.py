240aa8c0e26f9e6354a91a99850ab434608fd035
Artous <thibautartis@pm.me>
2019-10-19 16:24:45 +0200

    env should be ready

--- RL/gym-fantom_of_the_opera/gym_fantom_of_the_opera/envs/fantom_of_the_opera_env.py
+++ RL/gym-fantom_of_the_opera/gym_fantom_of_the_opera/envs/fantom_of_the_opera_env.py
@@ -2,29 +2,27 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-import game
-from game import Player
-from game import Game
+import fantom_game
+from fantom_game import Player
+from fantom_game import Game
 import agent_fantom as fantom
 import agent_inspector as inspector
-
+import game_globals
 
 class FooEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
-    self.nb_session = 0
     self.reset()
 
   def step(self, action):
-    self.game.step(action)
-    return self.game.game_state, self.game.get_reward(), self.game.done, []
+    return self.game.step(action)
 
   def reset(self):
     players = [Player(0, inspector.PlayerInspector(self.nb_session)), Player(
         1, fantom.PlayerFantom(self.nb_session))]
     self.game = Game(players)
-    self.nb_session += 1
+    game_globals.reset()
 
 
   def render(self, mode='human', close=False):
